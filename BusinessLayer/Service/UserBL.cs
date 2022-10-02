7789e91665c58b2022ef25e826afe6083e5c04dc
BhagyashreeNemade <108821937+BhagyashreeNemade@users.noreply.github.com>
2022-10-02 21:52:46 +0530

    initilize the project and project stucture

---
+++ BusinessLayer/Service/UserBL.cs
@@ -0,0 +1,30 @@
+﻿using BusinessLayer.Interface;
+using CommonLayer.Model;
+using RepositoryLayer.Entity;
+using RepositoryLayer.Interface;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BusinessLayer.Service
+{
+    public class UserBL : IUserBL
+    {
+        private readonly IUserInterfaceRL userInterfaceRL;
+        public UserBL(IUserInterfaceRL userInterfaceRL)
+        {
+            this.userInterfaceRL = userInterfaceRL;
+        }
+        public UserEntity UserRegitrations(UserRegistration userRegistration)
+        {
+            try
+            {
+                return userInterfaceRL.UserRegitrations(userRegistration);
+            }
+            catch (Exception e)
+            {
+                throw e;
+            }
+        }
+    }
+}
---
+++ FundooApplication/Controllers/UserController.cs
@@ -0,0 +1,42 @@
+﻿using BusinessLayer.Interface;
+using CommonLayer.Model;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+
+namespace FundooApplication.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+            private readonly IUserBL userBL;
+            public UserController(IUserBL userBL)
+            {
+                this.userBL = userBL;
+            }
+
+            //[Route("Register")]
+            [HttpPost("Register")]
+            public IActionResult Regispostration(UserRegistration userRegistration)
+            {
+                try
+                {
+                    var result = userBL.UserRegitrations(userRegistration);
+                    if (result != null)
+                    {
+                        return this.Ok(new { success = true, message = "User Registration Succesfull", data = result });
+                    }
+                    else
+                    {
+                        return this.BadRequest(new { success = false, message = "User Registration UnSuccesfull" });
+                    }
+                }
+                catch (Exception)
+                {
+                    throw;
+                }
+            }
+        }
+}
+
